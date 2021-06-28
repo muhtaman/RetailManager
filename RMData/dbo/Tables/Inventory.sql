24c416d1ed8206a8419480152be304e4f59a418c
muhtaman <muhtadi.munshi@gmail.com>
2021-06-28 16:36:38 -0400

    Register DB Tables
    The tables for the basic register system

---
+++ RMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductID] INT NULL, 
+    [Quantity] INT NULL, 
+    [PurchasePrice] MONEY NULL, 
+    [PurchaseDate] DATETIME2 NULL DEFAULT getutcdate(), 
+)
